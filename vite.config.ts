f0befdb03ae60c2bf46968c6fdb2f53caba86355
feraquzzi <mosesobembe@gmail.com>
2026-09-10 10:30:57 +0100

    change ports

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
-    port: 5174,
+    port: 5173,
   },
-})
+});
